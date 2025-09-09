dbde0f0860558d080953347c56d440e1263be33a
Prince jose <125237823+Princejose007@users.noreply.github.com>
2025-09-09 11:43:30 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,30 @@
+a=10
+console.log(typeof a);    
+// #-----> To Check Datatype
+
+
+b=1.2
+console.log(typeof b);
+
+c="Hello"
+console.log(typeof c);
+
+
+d=true
+console.log(typeof d);
+
+
+var e=12
+
+e="ab"
+
+// by using var we can change the datatype of variable 
+// anywhere in the program it is used  before 2015
+
+
+function sum()
+{
+    let x=10
+}
+
+console.log(x);    //this will give error because let is block scope variable we can use it outside the function
